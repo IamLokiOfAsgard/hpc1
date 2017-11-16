728ec2664464abd9a214320f1a8ab5ccc077f171
SBN <snaravagol@gmail.com>
2017-11-16 23:38:09 +0530

    Update cuda2.cu

--- cuda2.cu
+++ cuda2.cu
@@ -1,70 +1,52 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <math.h>   
-// CUDA kernel. Each thread takes care of one element of c 
-__global__ void vecAdd(double *a, double *b, double *c, int n) 
-{     
-// Get our global thread ID     
-int id = blockIdx.x*blockDim.x+threadIdx.x;       
-// Make sure we do not go out of bounds     
-if (id < n)         
-c[id] = a[id] + b[id]; 
-}   
-int main( int argc, char* argv[] ) 
-{     
-// Size of vectors     
-int n = 100;       
-// Host input vectors     
-double *h_a;     
-double *h_b;     
-//Host output vector     
-double *h_c;       
-// Device input vectors     
-double *d_a;     
-double *d_b;     
-//Device output vector     
-double *d_c;       
-// Size, in bytes, of each vector     
-size_t bytes = n*sizeof(double);       
-// Allocate memory for each vector on host     
-h_a = (double*)malloc(bytes);     
-h_b = (double*)malloc(bytes);     
-h_c = (double*)malloc(bytes);       
-// Allocate memory for each vector on GPU     
-cudaMalloc(&d_a, bytes);     
-cudaMalloc(&d_b, bytes);     
-cudaMalloc(&d_c, bytes);       
-int i;     
-// Initialize vectors on host     
-for( i = 0; i < n; i++ ) 
-{         
-h_a[i] = i;         
-h_b[i] = i;     
-}       
-// Copy host vectors to device     
-cudaMemcpy( d_a, h_a, bytes, cudaMemcpyHostToDevice); 
-    cudaMemcpy( d_b, h_b, bytes, cudaMemcpyHostToDevice);       
-int blockSize, gridSize;       
-// Number of threads in each thread block     
-blockSize = 1024;       
-// Number of thread blocks in grid     
-gridSize = (int)ceil((float)n/blockSize);       
-// Execute the kernel     
-vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);       
-// Copy array back to host     
-cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );       
-// Sum up vector c and print result divided by n, this should equal 1 within error     
-double sum = 0;     
-for(i=0; i<n; i++)         
-printf(" %f + %f =%f\n",h_a[i],h_b[i],h_c[i]);     
-//printf("final result: %f\n", sum/(double)n);       
-// Release device memory     
-cudaFree(d_a);     
-cudaFree(d_b);     
-cudaFree(d_c);       
-// Release host memory     
-free(h_a);     
-free(h_b);     
-free(h_c);       
-return 0;
- } 
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void add(int* d_vec1, int* d_vec2, int* d_vec3)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	d_vec3[idx] = d_vec2[idx] + d_vec1[idx];
+}
+
+int main()
+{
+	int i ;
+
+	int num_blocks = 1000;
+	int num_threads = 512;
+
+	int SIZE = num_threads*num_blocks;
+	int BYTES = SIZE * sizeof(int);
+
+	// declare device and host variables
+	int h_vec1[SIZE],h_vec2[SIZE],h_vec3[SIZE];
+	int *d_vec1, *d_vec2, *d_vec3;
+
+	// allocate memory on the device
+	cudaMalloc((void**)&d_vec1,BYTES);
+	cudaMalloc((void**)&d_vec2,BYTES);
+	cudaMalloc((void**)&d_vec3,BYTES);
+
+	// generate array on host
+	for(i=0;i<SIZE;i++)
+	{
+		h_vec1[i] = rand()%20;
+		h_vec2[i] = rand()%20;
+		h_vec3[i] = 0;
+	}
+
+	// move variables from host to device
+	cudaMemcpy(d_vec1,h_vec1,BYTES,cudaMemcpyHostToDevice);
+	cudaMemcpy(d_vec2,h_vec2,BYTES,cudaMemcpyHostToDevice);
+
+	// lauch kernel
+	add<<<num_blocks,num_threads>>>(d_vec1,d_vec2,d_vec3);
+
+	// move result back to main memory
+	cudaMemcpy(h_vec3,d_vec3,BYTES,cudaMemcpyDeviceToHost);
+
+	//print result
+	for(i=0;i<SIZE;i++)
+		printf("%d ", h_vec3[i]);
+
+}
